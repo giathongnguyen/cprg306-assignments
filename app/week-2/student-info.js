c91380904a4d3d272fd9bafc93316be85c868dcb
Thong Nguyen <giathong3012005@gmail.com>
2024-09-11 16:26:12 -0600

    Assignment-2 code

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Nguyen Gia Thong</h1>
+      <nav>
+        <Link href="https://github.com/giathongnguyen/cprg306-assignments">https://github.com/giathongnguyen/cprg306-assignments</Link>
+      </nav>
+    </div>
+  );
+}
