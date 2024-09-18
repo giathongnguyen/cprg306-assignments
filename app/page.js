6cc5383080f92e414e895ea7e5024cb089aa80e6
Thong Nguyen <giathong3012005@gmail.com>
2024-09-18 16:20:32 -0600

    Week-3 assignment

--- app/page.js
+++ app/page.js
@@ -2,12 +2,11 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <ul>
-        <li>
-          <Link href="/week-2">Week 2 Assignment</Link>
-        </li>
+    <div className='m-5 text-center'>
+      <h1 className='text-4xl'>CPRG 306: Web Development 2 - Assignments</h1>
+      <ul className='m-4 text-3xl'>
+        <li className='hover:text-green-400 hover:underline'><Link href="/week-2">Week 2 Assignment</Link></li>
+        <li className='hover:text-green-400 hover:underline'><Link href="/week-3">Week 3 Assignment</Link></li>
       </ul>
     </div>
   );
---
+++ app/week-3/item.js
@@ -0,0 +1,8 @@
+export default function Item({ name, quantity, category }) {
+    return (
+        <section className="m-4 p-2 bg-slate-500 w-96">
+            <h1 className="text-2xl font-bold">{name}</h1>
+            <p>Buy {quantity} in {category}</p>
+        </section>
+    );
+}
